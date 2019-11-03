b80d0d81b4c7764c9d3ffe550b009688bafbc94b
Salal Aslam <salalaslam@yahoo.com>
2019-11-03 15:24:13 +0500

    vendor mint service

---
+++ vendor/mint/service/src/routes/api.php
@@ -0,0 +1,15 @@
+<?php
+Route::group(['prefix' => 'api'], function () {
+    Route::get('/install/pre-requisite', 'Mint\Service\Controllers\InstallController@preRequisite');
+    Route::post('/install/validate/{option}', 'Mint\Service\Controllers\InstallController@store');
+    Route::post('/install', 'Mint\Service\Controllers\InstallController@store');
+    Route::post('/license', 'Mint\Service\Controllers\LicenseController@verify');
+
+    Route::get('/about', 'Mint\Service\Controllers\HomeController@about');
+    Route::get('/support', 'Mint\Service\Controllers\SupportController@index');
+    Route::post('/support', 'Mint\Service\Controllers\SupportController@submit');
+    Route::get('/update', 'Mint\Service\Controllers\UpdateController@index');
+    Route::post('/download', 'Mint\Service\Controllers\UpdateController@download');
+    Route::post('/update', 'Mint\Service\Controllers\UpdateController@update');
+    Route::post('/help/content', 'Mint\Service\Controllers\HomeController@helpDoc');
+});
