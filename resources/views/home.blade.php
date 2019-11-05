d318c3cffbee8cdd8d7eb319e637d5ad9c12b898
Salal Aslam <salalaslam@yahoo.com>
2019-11-05 07:07:13 +0500

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="description" content="">
         <meta name="author" content="Spark">
-        <title>{{config('app.name') ? : 'InstiKit'}}</title>
+        <title>{{config('app.name') ? : 'Spark'}}</title>
         <meta name="csrf-token" content="{{ csrf_token() }}" />
         <link rel="shortcut icon" href="/images/favicon.png">
         @if((!cache('direction') && config('config.direction') == 'rtl') || cache('direction') == 'rtl')
