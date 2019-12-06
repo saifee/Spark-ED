69145924c0fd0a742cd235c311d45160933ed066
Salal Aslam <salalaslam@yahoo.com>
2019-12-06 20:52:52 +0500

    stock sale module files

---
+++ resources/js/views/inventory/stock-sale/form.vue
@@ -0,0 +1,233 @@
+<template>
+	<div>
+        <form @submit.prevent="proceed" @keydown="stockSaleForm.errors.clear($event.target.name)">
+            <div class="row">
+                <div class="col-12">
+                    <div class="form-group">
+                        <label for="">{{trans('inventory_sale.stock_sale_date')}}</label>
+                        <datepicker v-model="stockSaleForm.date" :bootstrapStyling="true" @selected="stockSaleForm.errors.clear('date')" :placeholder="trans('inventory_sale.stock_sale_date')"></datepicker>
+                        <show-error :form-name="stockSaleForm" prop-name="date"></show-error>
+                    </div>
+                </div>
+                <div class="col-12">
+                    <div class="form-group" v-if="stockSaleForm.type == 'student'">
+                        <label for="">{{trans('student.student')}}</label>
+                        <v-select label="name" v-model="selected_student" name="student_id" id="student_id" :options="students" :placeholder="trans('student.select_student')" @select="onStudentSelect" @close="stockSaleForm.errors.clear('student_id')" @remove="stockSaleForm.student_id = ''">
+                            <div class="multiselect__option" slot="afterList" v-if="!students.length">
+                                {{trans('general.no_option_found')}}
+                            </div>
+                        </v-select>
+                        <show-error :form-name="stockSaleForm" prop-name="student_id"></show-error>
+                    </div>
+                </div>
+                <div class="col-12">
+                    <div class="form-group">
+                        <label for="">{{trans('inventory_sale.stock_sale_description')}}</label>
+                        <autosize-textarea v-model="stockSaleForm.description" rows="1" name="description" :placeholder="trans('inventory_sale.stock_sale_description')"></autosize-textarea>
+                        <show-error :form-name="stockSaleForm" prop-name="description"></show-error>
+                    </div>
+                </div>
+            </div>
+            <div class="p-t-20 border-top">
+                <div class="row" v-for="(detail, index) in stockSaleForm.details">
+                    <div class="col-12 col-sm-3">
+                        <div class="form-group">
+                            <label for="">
+                                {{trans('inventory.stock_item')}}
+                                <button type="button" class="btn btn-xs btn-danger" :key="`${index}_delete_detail`" v-confirm="{ok: confirmDelete(index)}" v-tooltip="trans('general.delete')"><i class="fas fa-times"></i></button>
+                            </label>
+                            <v-select label="name" v-model="detail.selected_stock_item" :name="getStockItemName(index)" :id="getStockItemName(index)" :options="stock_items" :placeholder="trans('inventory.select_stock_item')" @select="onStockItemSelect" @close="stockSaleForm.errors.clear(getStockItemName(index))" @remove="onStockItemRemove">
+                                <div class="multiselect__option" slot="afterList" v-if="!stock_items.length">
+                                    {{trans('general.no_option_found')}}
+                                </div>
+                            </v-select>
+                            <show-error :form-name="stockSaleForm" :prop-name="getStockItemName(index)"></show-error>
+                        </div>
+                    </div>
+                    <div class="col-12 col-sm-3">
+                        <div class="form-group">
+                            <label for="">{{trans('inventory_sale.stock_sale_quantity')}}</label>
+                            <input class="form-control" type="text" v-model="detail.quantity" :name="getQuantityName(index)" :placeholder="trans('inventory_sale.stock_sale_quantity')">
+                            <show-error :form-name="stockSaleForm" :prop-name="getQuantityName(index)"></show-error>
+                        </div>
+                    </div>
+                    <div class="col-12 col-sm-6">
+                        <div class="form-group">
+                            <label for="">
+                                {{trans('inventory.stock_item_description')}}
+                            </label>
+                            <input class="form-control" type="text" v-model="detail.description" :name="getDescriptionName(index)" :placeholder="trans('inventory.stock_item_description')">
+                            <show-error :form-name="stockSaleForm" :prop-name="getDescriptionName(index)"></show-error>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12">
+                        <div class="form-group">
+                            <button type="button" @click="addRow" class="btn btn-info btn-sm waves-effect waves-light">{{trans('inventory.add_new_stock_item')}}</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card-footer text-right">
+                <button v-show="id" type="button" class="btn btn-danger " @click="$router.push('/inventory/stock/sale')">{{trans('general.cancel')}}</button>
+                <button v-if="!id" type="button" class="btn btn-danger " @click="$emit('cancel')">{{trans('general.cancel')}}</button>
+                <button type="submit" class="btn btn-info waves-effect waves-light">{{trans('general.save')}}</button>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        components: {},
+        props: ['id'],
+        data(){
+            return {
+                stockSaleForm: new Form({
+                    transfer_type: 'sale',
+                    type: 'student',
+                    date: '',
+                    student_id: '',
+                    description: '',
+                    details: [],
+                    upload_token: ''
+                }),
+                stock_items: [],
+                students: [],
+                selected_student: null,
+                module_id: '',
+                clearAttachment: true
+            }
+        },
+        mounted(){
+            if(!this.id)
+                this.addRow();
+
+            if(this.id)
+                this.get();
+            else
+                this.stockSaleForm.upload_token = this.$uuid.v4();
+
+            this.getPreRequisite();
+        },
+        methods: {
+            proceed(){
+                if(this.id)
+                    this.update();
+                else
+                    this.store();
+            },
+            getPreRequisite(){
+                let loader = this.$loading.show();
+                axios.get('/api/stock/transfer/pre-requisite')
+                    .then(response => {
+                        this.students = response.students;
+                        this.stock_items = response.stock_items;
+                        loader.hide();
+                    })
+                    .catch(error => {
+                        loader.hide();
+                        helper.showErrorMsg(error);
+                    })
+            },
+            addRow(){
+                let new_index = this.stockSaleForm.details.push({
+                    quantity: '',
+                    stock_item_id: '',
+                    description: '',
+                    selected_stock_item: null
+                })
+            },
+            getStockItemName(index){
+                return index+'_stock_item_id';
+            },
+            getDescriptionName(index){
+                return index+'_description';
+            },
+            getQuantityName(index){
+                return index+'_quantity';
+            },
+            get(){
+                let loader = this.$loading.show();
+                axios.get('/api/stock/transfer/'+this.id)
+                    .then(response => {
+                        this.stockSaleForm.type = response.stock_transfer.type;
+                        this.module_id = response.stock_transfer.id;
+                        this.stockSaleForm.number = response.stock_transfer.number;
+                        this.stockSaleForm.date = response.stock_transfer.date;
+                        this.stockSaleForm.description = response.stock_transfer.description;
+                        this.stockSaleForm.student_id = response.stock_transfer.student_id;
+                        this.selected_student = response.selected_student;
+                        response.stock_transfer.details.forEach(detail => {
+                            this.stockSaleForm.details.push({
+                                quantity: detail.quantity,
+                                stock_item_id: detail.stock_item_id,
+                                selected_stock_item: (detail.stock_item_id) ? {id: detail.stock_item_id, name: detail.item.name} : null,
+                                description: detail.description
+                            });
+                        });
+                        loader.hide();
+                    })
+                    .catch(error => {
+                        loader.hide();
+                        helper.showErrorMsg(error);
+                    })
+            },
+            store(){
+                let loader = this.$loading.show();
+                this.stockSaleForm.date = helper.toDate(this.stockSaleForm.date);
+                this.stockSaleForm.post('/api/stock/transfer')
+                    .then(response => {
+                        toastr.success(response.message);
+                        this.selected_student = null;
+                        this.stockSaleForm.details = [];
+                        this.clearAttachment = !this.clearAttachment;
+                        this.addRow();
+                        this.$emit('completed');
+                        loader.hide();
+                    })
+                    .catch(error => {
+                        loader.hide();
+                        helper.showErrorMsg(error);
+                    })
+            },
+            update(){
+                let loader = this.$loading.show();
+                this.stockSaleForm.date = helper.toDate(this.stockSaleForm.date);
+                this.stockSaleForm.patch('/api/stock/transfer/'+this.id)
+                    .then(response => {
+                        toastr.success(response.message);
+                        loader.hide();
+                        this.$router.push('/inventory/stock/sale');
+                    })
+                    .catch(error => {
+                        loader.hide();
+                        helper.showErrorMsg(error);
+                    })
+            },
+            onStudentSelect(selectedOption){
+                this.stockSaleForm.student_id = selectedOption.id;
+            },
+            confirmDelete(index){
+                return dialog => this.deleteDetail(index);
+            },
+            deleteDetail(index){
+                this.stockSaleForm.details.splice(index, 1);
+            },
+            onStockItemSelect(selectedOption, id){
+                let index = id.split("_")[0];
+                let detail = this.stockSaleForm.details[index];
+                detail.stock_item_id = selectedOption.id;
+            },
+            onStockItemRemove(removedOption, id){
+                let index = id.split("_")[0];
+                let detail = this.stockSaleForm.details[index];
+                detail.stock_item_id = '';
+            }
+        },
+        computed:{
+        }
+    }
+</script>
---
+++ resources/js/views/inventory/stock-sale/show.vue
@@ -0,0 +1,163 @@
+<template>
+    <div v-if="stock_sale.id">
+        <div class="page-titles">
+            <div class="row">
+                <div class="col-12 col-sm-6">
+                    <h3 class="text-themecolor">{{trans('inventory_sale.stock_sale_detail')}}
+                        <span class="card-subtitle">{{'#'+stock_sale.id}}</span>
+                    </h3>
+                </div>
+                <div class="col-12 col-sm-6">
+                    <div class="action-buttons pull-right">
+                        <router-link to="/inventory/stock/sale" class="btn btn-info btn-sm"><i class="fas fa-list"></i> <span class="d-none d-sm-inline">{{trans('inventory_sale.stock_sale')}}</span></router-link>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12 col-sm-6 pr-0">
+                	<div class="card border-right">
+                		<div class="card-body">
+                            <div class="table-responsive px-2">
+                                <table class="table table-sm custom-show-table">
+                                    <tbody>
+                                        <tr>
+                                            <td>{{trans('inventory_sale.stock_sale_detail')}}</td>
+                                            <td>
+                                                <template v-if="stock_sale.type == 'student'">
+                                                    {{trans('student.student_name')+': '+getStudentName(stock_sale.student)}} <br />
+                                                    {{trans('student.father_name')+': '+stock_sale.student.parent.father_name}} <br />
+                                                    {{trans('student.contact_number')+': '+stock_sale.student.contact_number}} <br />
+                                                </template>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                        	<td>{{trans('inventory_sale.stock_sale_date')}}</td>
+                                        	<td>{{stock_sale.date | moment}}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>{{trans('inventory_sale.stock_sale_description')}}</td>
+                                            <td>{{stock_sale.description}}</td>
+                                        </tr>
+                                        <tr>
+                                            <td colspan="2">
+                                                <div v-if="attachments.length">
+                                                    <ul class="m-t-10 upload-file-list">
+                                                        <li class="upload-file-list-item" v-for="attachment in attachments">
+                                                            <a :href="`/stock/sale/${stock_sale.id}/attachment/${attachment.uuid}/download?token=${authToken}`" class="no-link-color"><i :class="['file-icon', 'fas', 'fa-lg', attachment.file_info.icon]"></i> <span class="upload-file-list-item-size">{{attachment.file_info.size}}</span> {{attachment.user_filename}}</a>
+                                                        </li>
+                                                    </ul>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <h4 class="card-title px-3">{{trans('inventory.stock_item')}}</h4>
+                            <div class="table-responsive px-2">
+                                <table class="table table-sm">
+                                	<thead>
+                                		<tr>
+                                			<th class="p-l-20">{{trans('inventory.stock_item')}}</th>
+                                			<th>{{trans('inventory_sale.stock_sale_quantity')}}</th>
+                                			<th>{{trans('inventory.stock_item_description')}}</th>
+                                		</tr>
+                                	</thead>
+                                	<tbody>
+                                		<tr v-for="detail in stock_sale.details">
+                                			<td class="p-l-20">{{detail.item.name}}</td>
+                                			<td>{{detail.quantity}}</td>
+                                			<td>{{detail.description}}</td>
+                                		</tr>
+                                	</tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import returnForm from './return-form'
+
+	export default {
+        components: {returnForm},
+        data(){
+        	return {
+                id:this.$route.params.id,
+        		stock_sale: {},
+                attachments: []
+        	}
+        },
+        mounted() {
+        	this.get();
+        },
+        methods: {
+            hasPermission(permission){
+                return helper.hasPermission(permission);
+            },
+        	get(){
+                let loader = this.$loading.show();
+                axios.get('/api/stock/transfer/'+this.id)
+                    .then(response => {
+                    	this.stock_sale = response.stock_transfer;
+                        this.attachments = response.attachments;
+                    	loader.hide();
+                    })
+                    .catch(error => {
+                        loader.hide();
+                    	helper.showErrorMsg(error);
+                        this.$router.push('/inventory/stock/sale');
+                    })
+        	},
+            getStudentName(student){
+                return helper.getStudentName(student);
+            },
+            formatCurrency(amount) {
+                return helper.formatCurrency(amount);
+            },
+            deleteReturn(return_detail){
+                let loader = this.$loading.show();
+                axios.delete('/api/stock/transfer/'+this.stock_sale.id+'/return/'+return_detail.id)
+                    .then(response => {
+                        toastr.success(response.message);
+                        this.get();
+                        loader.hide();
+                    }).catch(error => {
+                        loader.hide();
+                        helper.showErrorMsg(error);
+                    });
+            },
+            getCount(item, status) {
+                let count = 0;
+                this.stock_sale.return_details.forEach(return_detail => {
+                    if (return_detail.stock_item_id == item.id && return_detail.type == status) {
+                        count = count + 1;
+                    }
+                })
+
+                return count;
+            }
+        },
+        filters: {
+          moment(date) {
+            return helper.formatDate(date);
+          },
+          momentDateTime(date) {
+            return helper.formatDateTime(date);
+          },
+          momentTime(time) {
+            return helper.formatTime(time);
+          }
+        },
+        computed: {
+            authToken(){
+                return helper.getAuthToken();
+            }
+        }
+	}
+</script>
