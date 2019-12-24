e42b1822ce88777a0ffb0b3c29d27f631408a23d
Salal Aslam <salalaslam@yahoo.com>
2019-12-24 14:39:26 +0500

    Update social.vue

--- resources/js/views/configuration/authentication/social.vue
+++ resources/js/views/configuration/authentication/social.vue
@@ -14,6 +14,7 @@
                     <div class="form-group">
                         <label for=""><small>{{trans('auth_social.social_login_google_client_id')}}</small></label>
                         <input class="form-control" type="text" v-model="configForm.social_login_google_client_id" name="social_login_google_client_id" :placeholder="trans('auth_social.social_login_google_client_id')">
+                        <small class="form-text text-muted"><a target="_blank" href="https://developers.google.com/identity/sign-in/web/sign-in">click here</a> to get client id</small>
                         <show-error :form-name="configForm" prop-name="social_login_google_client_id"></show-error>
                     </div>
                 </div>
@@ -31,6 +32,7 @@
                     <div class="form-group">
                         <label for=""><small>{{trans('auth_social.social_login_microsoft_client_id')}}</small></label>
                         <input class="form-control" type="text" v-model="configForm.social_login_microsoft_client_id" name="social_login_microsoft_client_id" :placeholder="trans('auth_social.social_login_microsoft_client_id')">
+                        <small class="form-text text-muted"><a target="_blank" href="https://docs.microsoft.com/en-us/azure/active-directory/develop/quickstart-v2-javascript">click here</a> to get client id</small>
                         <show-error :form-name="configForm" prop-name="social_login_microsoft_client_id"></show-error>
                     </div>
                 </div>
