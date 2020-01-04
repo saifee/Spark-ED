3e0afbbc2f5ee1c5bb20d54b22ac677f1ae24908
Salal Aslam <salalaslam@yahoo.com>
2020-01-04 15:47:59 +0500

    Create WalletPaymentRequest.php

---
+++ app/Http/Requests/student/WalletPaymentRequest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Requests\Student;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class WalletPaymentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'amount'            => 'required|numeric',
+            'date'              => 'required|date'
+        ];
+    }
+
+    /**
+     * Translate fields with user friendly name.
+     *
+     * @return array
+     */
+    public function attributes()
+    {
+        return [
+            'amount'            => trans('finance.amount'),
+            'date'              => trans('finance.date_of_payment')
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+        ];
+    }
+}
