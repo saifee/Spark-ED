83387a395d8d8ed3e6b1992ef5828385d18e97d8
Salal Aslam <salalaslam@yahoo.com>
2020-01-04 15:51:13 +0500

    Create student_wallet.php

---
+++ resources/lang/en/student_wallet.php
@@ -0,0 +1,12 @@
+<?php return array (
+  'student_wallet' => 'Wallet',
+  'student_wallet_balance' => 'Wallet Balance',
+  'student_wallet_transactions' => 'Wallet Transactions',
+  'view_student_wallet' => 'View Student Wallet',
+  'date' => 'date',
+  'debit' => 'debit',
+  'credit' => 'credit',
+  'description' => 'description',
+  'details' => 'Details',
+  'transfer_details' => 'View Transfer Details',
+);
