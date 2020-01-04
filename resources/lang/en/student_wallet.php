5e61699ea555aadc9e4c47aa893990a59f7b9822
Salal Aslam <salalaslam@yahoo.com>
2020-01-04 17:15:00 +0500

    Update student_wallet.php

--- resources/lang/en/student_wallet.php
+++ resources/lang/en/student_wallet.php
@@ -1,5 +1,7 @@
 <?php return array (
   'student_wallet' => 'Wallet',
+  'enable_student_wallet' => 'Enable Student Wallet',
+  'student_wallet_limit' => 'Student Wallet Limit',
   'student_wallet_balance' => 'Wallet Balance',
   'student_wallet_transactions' => 'Wallet Transactions',
   'view_student_wallet' => 'View Student Wallet',
