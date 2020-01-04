2974cc189c41db52cd5acc802d1607a1717758ce
Salal Aslam <salalaslam@yahoo.com>
2020-01-04 17:14:06 +0500

    Create WalletStatusRequest.php

---
+++ app/Http/Requests/student/WalletStatusRequest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Requests\Student;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserLoginRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $rules = [
+        ];
+        return $rules;
+    }
+
+    /**
+     * Translate fields with user friendly name.
+     *
+     * @return array
+     */
+    public function attributes()
+    {
+        return [
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [];
+    }
+}
