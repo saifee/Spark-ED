170debfbfc2766e4221e7b38e5d4c826f121b2ed
Salal Aslam <salalaslam@yahoo.com>
2020-01-20 15:03:00 +0500

    Update cache.php

--- config/cache.php
+++ config/cache.php
@@ -98,6 +98,7 @@ return [
     |
     */
 
-    'prefix' => env('CACHE_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_cache'),
+    // 'prefix' => env('CACHE_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_cache'),
+    'prefix' => str_replace('.','',getHostNameForCachePrefix()).'_cache',
 
 ];
