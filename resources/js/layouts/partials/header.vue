fb17645f647181986aed99fa3c7702165312d404
Salal Aslam <salalaslam@yahoo.com>
2020-01-28 00:04:00 +0500

    Update header.vue

--- resources/js/layouts/partials/header.vue
+++ resources/js/layouts/partials/header.vue
@@ -19,7 +19,7 @@
                     <li class="nav-item d-none d-sm-inline" v-if="getConfig('maintenance_mode')"><span class="mt-4 badge badge-danger m-b-10">{{trans('configuration.under_maintenance')}}</span></li>
                     <li class="nav-item d-none d-sm-inline" v-if="!getConfig('mode')"><span class="mt-4 badge badge-danger m-b-10">{{trans('configuration.test_mode')}}</span></li>
 
-                    <li class="nav-item">{{getConfig('institute_name')}}</li>
+                    <li class="nav-item" style="color: #fff; font-size: 20px; font-weight: 100; padding: 18px 10px; letter-spacing: 2px; text-transform: uppercase;">{{getConfig('institute_name')}}</li>
                 </ul>
                 <ul class="navbar-nav flex-filler"></ul>
                 <ul class="navbar-nav my-lg-0">
