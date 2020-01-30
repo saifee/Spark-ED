f4a2c4340a78ffb11647cf166f618bd3397ca08e
Salal Aslam <salalaslam@yahoo.com>
2020-01-31 01:01:14 +0500

    Update spark.vue

--- resources/js/views/configuration/system/spark.vue
+++ resources/js/views/configuration/system/spark.vue
@@ -2,16 +2,12 @@
     <div class="row">
         <div class="col-12 col-sm-6">
             <div class="form-group">
-                <label for="">{{trans('configuration_spark.replace_sidebar_menu_with_top_menu')}}</label>
-                <div>
-                    <switches class="" v-model="configForm.replace_sidebar_menu_with_top_menu" theme="bootstrap" color="success"></switches>
-                </div>
+                <v-switch v-model="configForm.replace_sidebar_menu_with_top_menu" :label="trans('configuration_spark.replace_sidebar_menu_with_top_menu')" color="success" />
             </div>
         </div>
         <div class="col-12 col-sm-6">
             <div class="form-group">
-                <v-switch v-model="configForm.sidebar_clipped" :label="trans('configuration_spark.sidebar_clipped')">
-                </v-switch>
+                <v-switch v-model="configForm.sidebar_clipped" :label="trans('configuration_spark.sidebar_clipped')" color="success" />
             </div>
         </div>
     </div>
