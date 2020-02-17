6072a1bd5c0fe21acf9eb58d038a7916b4f31f4c
Salal Aslam <salalaslam@yahoo.com>
2020-02-18 01:01:18 +0500

    course_skills

---
+++ app/Models/configuration/academic/CourseSkill.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Models\Configuration\Academic;
+
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
+
+class CourseSkill extends Model
+{
+    use LogsActivity;
+
+    protected $fillable = [
+                            'course_id',
+                            'position',
+                            'name',
+                            'options',
+                            'academic_session_id'
+                            'positive'
+                            'points'
+                            'course_skill_icon_id'
+                        ];
+    protected $casts = ['options' => 'array'];
+    protected static $logName = 'course_skill';
+    protected static $logFillable = true;
+    protected static $logOnlyDirty = true;
+    protected static $ignoreChangedAttributes = ['updated_at'];
+
+    public function acadmicSession()
+    {
+        return $this->belongsTo('App\Models\Academic\AcademicSession');
+    }
+
+    public function course()
+    {
+        return $this->belongsTo('App\Models\Academic\Course');
+    }
+
+    public function getOption(string $option)
+    {
+        return array_get($this->options, $option);
+    }
+
+    public function scopeFilterByName($q, $name, $s = 0)
+    {
+        if (! $name) {
+            return $q;
+        }
+
+        return ($s) ? $q->where('name', '=', $name) : $q->where('name', 'like', '%'.$name.'%');
+    }
+
+    public function scopeFilterById($q, $id)
+    {
+        if (! $id) {
+            return $q;
+        }
+
+        return $q->where('id', '=', $id);
+    }
+
+    public function scopeFilterByAcademicSessionId($q, $academic_session_id)
+    {
+        if (! $academic_session_id) {
+            return $q;
+        }
+
+        return $q->where('academic_session_id', '=', $academic_session_id);
+    }
+
+    public function scopeFilterBySession($q, $session_id = null)
+    {
+        return $q->where('academic_session_id', '=', $session_id ? : config('config.default_academic_session.id'));
+    }
+
+    public function scopeFilterByName($q, $name, $s = 0)
+    {
+        if (! $name) {
+            return $q;
+        }
+
+        return ($s) ? $q->where('name', '=', $name) : $q->where('name', 'like', '%'.$name.'%');
+    }
+}
---
+++ database/migrations/2020_02_17_193622_create_course_skills_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCourseSkillsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('course_skills', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('position')->default(0);
+            $table->bigInteger('academic_session_id')->unsigned()->nullable();
+            $table->foreign('academic_session_id')->references('id')->on('academic_sessions')->onDelete('cascade');
+            $table->bigInteger('course_id')->unsigned()->nullable();
+            $table->foreign('course_id')->references('id')->on('courses')->onDelete('cascade');
+            $table->string('name')->nullable();
+            $table->integer('positive')->default(1);
+            $table->integer('points')->default(1);
+            $table->bigInteger('course_skill_icon_id')->unsigned()->nullable();
+            $table->foreign('course_skill_icon_id')->references('id')->on('course_skill_icons')->onDelete('set null');
+            $table->text('options')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('course_skills', function(Blueprint $table)
+        {
+            $table->dropForeign('course_skills_academic_session_id_foreign');
+            $table->dropForeign('course_skills_course_id_foreign');
+            $table->dropForeign('course_skills_course_skill_icon_id_foreign');
+        });
+
+        Schema::dropIfExists('course_skills');
+    }
+}
