5541c2cc9796bec39b90ddbb339d59096fdcc3ef
Salal Aslam <salalaslam@yahoo.com>
2020-02-20 01:06:30 +0500

    Update behaviour.php

--- resources/lang/en/behaviour.php
+++ resources/lang/en/behaviour.php
@@ -1,2 +1,11 @@
 <?php return array (
+  'add_new_skill' => 'Add New Skill',
+  'behaviour_configuration' => 'Behaviour Configuration',
+  'delete_skill' => 'Delete Skill',
+  'edit_skill' => 'Edit Skill',
+  'skill' => 'Skill',
+  'skill_module_description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam ipsam possimus inventore ab tempora commodi neque esse quidem earum animi repellendus laudantium, ipsum veniam sequi similique. Vel reiciendis, fuga similique!',
+  'skill_name' => 'Name',
+  'skill_points' => 'Points',
+  'skill_positive' => 'Positive',
 );
