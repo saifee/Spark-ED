d0a61fdfda203e01006661d21e9c57188f67fbee
Salal Aslam <salalaslam@yahoo.com>
2020-03-08 23:47:44 +0500

    updated skillrequest

--- app/Http/Requests/configuration/Behaviour/SkillRequest.php
+++ app/Http/Requests/configuration/Behaviour/SkillRequest.php
@@ -36,7 +36,7 @@ class SkillRequest extends FormRequest
     public function attributes()
     {
         return [
-            'name' => trans('academic.skill_name')
+            'name' => trans('behaviour.skill_name')
         ];
     }
 
