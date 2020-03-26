6e2387804f1f1df98a17f39d1a9d7c312578caa9
Salal Aslam <salalaslam@yahoo.com>
2020-03-27 01:43:29 +0500

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -20,7 +20,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \App\Http\Middleware\ScriptMint::class,
+        \App\Http\Middleware\Administrator::class,
         \App\Http\Middleware\RedirectSecureIfRequired::class,
         \App\Http\Middleware\XSSProtection::class,
         \App\Http\Middleware\IpRestricted::class
