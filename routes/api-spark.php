b880740427fe1c054f82494311270a285a867038
Salal Aslam <salalaslam@yahoo.com>
2020-04-06 00:32:56 +0500

    Update api-spark.php

--- routes/api-spark.php
+++ routes/api-spark.php
@@ -4,4 +4,5 @@ Route::group(['middleware' => ['auth:api']], function () {
   Route::resource('/behaviour/stories', 'Behaviour\StoryController');
   Route::resource('/behaviour/stories.likes', 'Behaviour\Story\LikeController');
   Route::resource('/behaviour/stories.comments', 'Behaviour\Story\CommentController');
+  Route::resource('/behaviour/messages', 'Behaviour\MessageController');
 });
