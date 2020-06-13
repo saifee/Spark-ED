2721a3004d6f46dbdad8968a14e6933308e484b1
Salal Aslam <salalaslam@yahoo.com>
2020-06-13 16:10:32 +0500

    Update calendar.vue

--- resources/js/views/calendar/calendar.vue
+++ resources/js/views/calendar/calendar.vue
@@ -15,6 +15,7 @@
 				events: [],
 				config: {
 					defaultView: 'month',
+					firstDay: helper.getDayInInteger(this.getConfig('first_day_of_week') || 'monday'),
 					locale: this.getConfig('locale'),
 					isRTL: this.getConfig('direction') == 'rtl' ? true : false,
 					eventRender: function(event, element) {
@@ -39,7 +40,7 @@
 						response.holidays.forEach(holiday => {
 							this.events.push({
 								title: holiday.description,
-								start: holiday.date,
+								start: helper.toDate(holiday.date),
 								icon: 'coffee',
 								color: 'teal'
 							})
@@ -47,16 +48,16 @@
 						response.todos.forEach(todo => {
 							this.events.push({
 								title: todo.title,
-								start: todo.date,
+								start: helper.toDate(todo.date),
 								icon: 'check-circle',
-								color: todo.date < moment().format('YYYY-MM-DD') ? 'red' : ''
+								color: todo.date < helper.today() ? 'red' : ''
 							})
 						})
 						response.events.forEach(event => {
 							this.events.push({
 								title: event.title,
-								start: event.start_date,
-								end: event.end_date,
+								start: helper.toDate(event.start_date),
+								end: helper.toDate(event.enddate),
 								icon: 'bullhorn',
 								color: 'purple'
 							})
