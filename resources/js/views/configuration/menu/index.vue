4d66e58b7dcb8f40421888bc963db33ce3c5feaf
Salal Aslam <salalaslam@yahoo.com>
2020-06-13 16:15:00 +0500

    Update index.vue

--- resources/js/views/configuration/menu/index.vue
+++ resources/js/views/configuration/menu/index.vue
@@ -3,7 +3,7 @@
         <div class="page-titles">
             <div class="row">
                 <div class="col-12 col-sm-6">
-                    <h3 class="text-themecolor">{{trans('configuration.menu')}}</h3>
+                    <h3 class="text-themecolor">{{trans('configuration.menu_configuration')}}</h3>
                 </div>
                 <div class="col-12 col-sm-6">
                     <div class="action-buttons pull-right">
@@ -14,112 +14,22 @@
             </div>
         </div>
         <div class="container-fluid">
-            <div class="card">
-                <div class="card-body p-4">
-                    <form @submit.prevent="submit" @keydown="configForm.errors.clear($event.target.name)">
-                        <div class="row">
-                            <div class="col-12 offset-md-1">
-                                <template v-for="module in configForm.modules">
-                                    <h4 class="card-title">{{trans(module.translation)}}</h4>
-                                    <div class="form-group">
-    	                                <label class="custom-control custom-checkbox">
-    	                                    <input type="checkbox" class="custom-control-input" value="1" v-model="module.menu.visibility" :name="getMenuName(module.menu.name)" @change="updateSubMenu(module.menu)">
-    	                                    <span class="custom-control-label">{{trans(module.menu.translation)}}</span>
-    	                                </label>
-                                    </div>
-                                    <div v-show="module.menu.visibility" class="form-group" style="padding-left: 40px;" v-for="submenu in module.menu.submenu">
-                                        <label class="custom-control custom-checkbox">
-                                            <input type="checkbox" class="custom-control-input" value="1" v-model="submenu.visibility" :name="getMenuName(submenu.name)">
-                                            <span class="custom-control-label">{{trans(submenu.translation)}}</span>
-                                        </label>
-                                    </div>
-                                </template>
-                            </div>
-                        </div>
-                        <button type="submit" class="btn btn-info waves-effect waves-light pull-right m-t-10">{{trans('general.save')}}</button>
-                    </form>
-                </div>
-            </div>
+            <menu-form></menu-form>
         </div>
+        <right-panel :topic="help_topic"></right-panel>
     </div>
 </template>
 
+
 <script>
-	export default {
-        components : { },
-        data() {
+    import menuForm from './form'
+
+    export default {
+        components: {menuForm},
+        data(){
             return {
-                configForm: new Form({
-                    config_type: 'menu',
-                    modules: []
-                },false),
-                menus: [],
                 help_topic: ''
             }
-        },
-        mounted(){
-            if(!helper.hasPermission('access-configuration')){
-                helper.notAccessibleMsg();
-                this.$router.push('/home');
-            }
-
-            this.getPreRequisite();
-        },
-        methods: {
-            getPreRequisite(){
-                let loader = this.$loading.show();
-                axios.get('/api/configuration/variable?type=menu')
-                    .then(response => {
-                        this.configForm.modules = response.modules;
-                        
-                        this.menus = response.menus;
-                        this.configForm.modules.forEach(module => {
-                            if (response.menus.findIndex(o => o === module.menu.name) >= 0) {
-                                module.menu.visibility = true;
-                            } else {
-                                module.menu.visibility = false;
-                            }
-
-                            module.menu.submenu.forEach(sbmenu => {
-                                if (response.menus.findIndex(o => o === sbmenu.name) >= 0) {
-                                    sbmenu.visibility = true;
-                                } else {
-                                    sbmenu.visibility = false;
-                                }
-                            })
-                        });
-                        loader.hide();
-                    }).catch(error => {
-                        loader.hide();
-                        helper.showErrorMsg(error);
-                    });
-            },
-            getMenuName(name) {
-                return 'show_'+name+'_menu';
-            },
-            updateSubMenu(menu){
-                if (! menu.visibility) {
-                    menu.submenu.forEach(sbmenu => {
-                        sbmenu.visibility = false;
-                    });
-                } else {
-                    menu.submenu.forEach(sbmenu => {
-                        sbmenu.visibility = true;
-                    });
-                }
-            },
-            submit(){
-                let loader = this.$loading.show();
-                this.configForm.post('/api/configuration')
-                    .then(response => {
-                        loader.hide();
-                        this.$store.dispatch('setConfig',{loaded: false});
-                        toastr.success(response.message);
-                    }).catch(error => {
-                        loader.hide();
-                        helper.showErrorMsg(error);
-                    });
-            }
         }
-	}
+    }
 </script>
