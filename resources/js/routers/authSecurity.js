651cd77bf5c532940e43f68b2ac0bf4c2d622c96
Salal Aslam <salalaslam@yahoo.com>
2020-06-13 15:02:52 +0500

    Update authSecurity.js

--- resources/js/routers/authSecurity.js
+++ resources/js/routers/authSecurity.js
@@ -1,10 +1,12 @@
 export default [
     {
         path: '/auth/security',
+        name: 'authSecurity',
         component: () => import(/* webpackChunkName: "js/auth/security" */ '@views/auth/security'),
     },
     {
         path: '/auth/lock',
+        name: 'authLock',
         component: () => import(/* webpackChunkName: "js/auth/lock" */ '@views/auth/lock'),
     }
 ]
