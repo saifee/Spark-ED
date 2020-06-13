37efde119271678fc102cc20d3e93d0b87ba0bbc
Salal Aslam <salalaslam@yahoo.com>
2020-06-13 16:07:32 +0500

    Update index.vue

--- resources/js/views/academic/certificate/index.vue
+++ resources/js/views/academic/certificate/index.vue
@@ -191,6 +191,8 @@
                 if (typeof page !== 'number') {
                     page = 1;
                 }
+                this.filter.date_of_certificate_start_date = helper.toDate(this.filter.date_of_certificate_start_date);
+                this.filter.date_of_certificate_end_date = helper.toDate(this.filter.date_of_certificate_end_date);
                 let url = helper.getFilterURL(this.filter);
                 axios.get('/api/certificate?page=' + page + url)
                     .then(response => {
