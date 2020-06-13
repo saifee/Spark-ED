f86ab2b5805a115f5c92a5ab4a627f3ec70507bb
Salal Aslam <salalaslam@yahoo.com>
2020-06-13 17:01:32 +0500

    Update index.vue

--- resources/js/views/student/promotion/index.vue
+++ resources/js/views/student/promotion/index.vue
@@ -48,13 +48,13 @@
                             </div>
                             <div class="col-12 col-sm-2">
                                 <div class="form-group">
-                                    <label for="">{{trans('student.first_guardian_name')}}</label>
+                                    <label for="">{{trans('student.father_name')}}</label>
                                     <input class="form-control" name="father_name" v-model="filter.father_name">
                                 </div>
                             </div>
                             <div class="col-12 col-sm-2">
                                 <div class="form-group">
-                                    <label for="">{{trans('student.second_guardian_name')}}</label>
+                                    <label for="">{{trans('student.mother_name')}}</label>
                                     <input class="form-control" name="mother_name" v-model="filter.mother_name">
                                 </div>
                             </div>
@@ -90,8 +90,8 @@
                                     </th>
                                     <th>{{trans('student.admission_number_short')}}</th>
                                     <th>{{trans('student.name')}}</th>
-                                    <th>{{trans('student.first_guardian_name')}}</th>
-                                    <th>{{trans('student.second_guardian_name')}}</th>
+                                    <th>{{trans('student.father_name')}}</th>
+                                    <th>{{trans('student.mother_name')}}</th>
                                     <th>{{trans('student.date_of_admission')}}</th>
                                     <th>{{trans('student.date_of_promotion')}}</th>
                                     <th>{{trans('academic.batch')}}</th>
@@ -108,8 +108,8 @@
                                     </td>
                                     <td v-text="getAdmissionNumber(student_record.admission)"></td>
                                     <td v-text="getStudentName(student_record.student)"></td>
-                                    <td v-text="student_record.student.parent.first_guardian_name"></td>
-                                    <td v-text="student_record.student.parent.second_guardian_name"></td>
+                                    <td v-text="student_record.student.parent.father_name"></td>
+                                    <td v-text="student_record.student.parent.mother_name"></td>
                                     <td>{{student_record.admission.date_of_admission | moment}}</td>
                                     <td>{{getDateOfPromotion(student_record)}}</td>
                                     <td v-text="student_record.batch.course.name+' '+student_record.batch.name"></td>
