c29d15bde90e7f680aefe95c47eeeeb77891f77f
Salal Aslam <salalaslam@yahoo.com>
2020-06-13 16:35:58 +0500

    Update index.vue

--- resources/js/views/inventory/stock-purchase/index.vue
+++ resources/js/views/inventory/stock-purchase/index.vue
@@ -164,6 +164,8 @@
                 if (typeof page !== 'number') {
                     page = 1;
                 }
+                this.filter.date_start_date = helper.toDate(this.filter.date_start_date);
+                this.filter.date_end_date = helper.toDate(this.filter.date_end_date);
                 let url = helper.getFilterURL(this.filter);
                 axios.get('/api/stock/purchase?page=' + page + url)
                     .then(response => {
