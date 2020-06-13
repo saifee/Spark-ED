37bc4826356f7bdb4d88e4a4d077f6f339631336
Salal Aslam <salalaslam@yahoo.com>
2020-06-13 16:45:50 +0500

    Update index.vue

--- resources/js/views/reception/call-log/index.vue
+++ resources/js/views/reception/call-log/index.vue
@@ -196,6 +196,8 @@
                 if (typeof page !== 'number') {
                     page = 1;
                 }
+                this.filter.date_start_date = helper.toDate(this.filter.date_start_date);
+                this.filter.date_end_date = helper.toDate(this.filter.date_end_date);
                 let url = helper.getFilterURL(this.filter);
                 axios.get('/api/call/log?page=' + page + url)
                     .then(response => {
