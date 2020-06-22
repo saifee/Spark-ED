d9f0236d6cfde8dc922bdc15b723b75386f09340
Salal Aslam <salalaslam@yahoo.com>
2020-06-22 08:40:09 +0500

    Update manage.vue

--- resources/js/views/utility/todo/manage.vue
+++ resources/js/views/utility/todo/manage.vue
@@ -1,10 +1,38 @@
 <template>
     <div>
+        <div class="page-titles">
+            <v-row>
+                <v-col cols="12" md="6">
+                    <h3 class="text-themecolor">{{trans('utility.edit_todo')}}</h3>
+                </v-col>
+                <v-col cols="12" md="6">
+                    <div class="action-buttons pull-right">
+                        <v-btn small color="primary" depressed @click="$router.push('/utility/todo')"><i class="fas fa-list"></i> <span class="d-none d-sm-inline">{{trans('utility.todo')}}</span></v-btn>
+                    </div>
+                </v-col>
+            </v-row>
+        </div>
+        <v-row class="row">
+            <v-col cols="12" md="6">
+                <tasks-form :id="id"></tasks-form>
+            </v-col>
+            <v-col cols="12" md="6">
+                <team-form :id="id"></team-form>
+            </v-col>
+            <v-col cols="12" md="6">
+                <skills-form :id="id"></skills-form>
+            </v-col>
+        </v-row>
     </div>
 </template>
 
 <script>
+    import tasksForm from './tasks';
+    import teamForm from './team';
+    import skillsForm from './skills';
+
     export default {
+        components : { tasksForm,teamForm,skillsForm, },
         data() {
             return {
                 id:this.$route.params.id
