10fab29b4bb3cd1f3de5f421d6b6b305071b2955
Salal Aslam <salalaslam@yahoo.com>
2020-10-11 11:50:27 +0500

    Update report.vue

--- resources/js/views/finance/report.vue
+++ resources/js/views/finance/report.vue
@@ -10,7 +10,7 @@
                         <div class="card-body">
                             <h4 class="card-title">{{trans('finance.fee_summary_report')}}</h4>
                             <p class="card-text font-80pc">{{trans('finance.fee_summary_report_module_description')}}</p>
-                            <router-link to="/finance/fee/report/summary" class="btn btn-info btn-sm">{{trans('general.go_to_link', {link: trans('finance.fee_summary_report')})}}</router-link>
+                            <v-btn to="/finance/fee/report/summary" x-small color="#009efb" dark depressed>{{trans('general.go_to_link', {link: trans('finance.fee_summary_report')})}}</v-btn>
                         </div>
                     </div>
                 </div>
@@ -19,7 +19,7 @@
                         <div class="card-body">
                             <h4 class="card-title">{{trans('finance.fee_concession_report')}}</h4>
                             <p class="card-text font-80pc">{{trans('finance.fee_concession_report_module_description')}}</p>
-                            <router-link to="/finance/fee/report/concession" class="btn btn-info btn-sm">{{trans('general.go_to_link', {link: trans('finance.fee_concession_report')})}}</router-link>
+                            <v-btn to="/finance/fee/report/concession" x-small color="#009efb" dark depressed>{{trans('general.go_to_link', {link: trans('finance.fee_concession_report')})}}</v-btn>
                         </div>
                     </div>
                 </div>
@@ -28,7 +28,7 @@
                         <div class="card-body">
                             <h4 class="card-title">{{trans('finance.fee_due_report')}}</h4>
                             <p class="card-text font-80pc">{{trans('finance.fee_due_report_module_description')}}</p>
-                            <router-link to="/finance/fee/report/due" class="btn btn-info btn-sm">{{trans('general.go_to_link', {link: trans('finance.fee_due_report')})}}</router-link>
+                            <v-btn to="/finance/fee/report/due" x-small color="#009efb" dark depressed>{{trans('general.go_to_link', {link: trans('finance.fee_due_report')})}}</v-btn>
                         </div>
                     </div>
                 </div>
@@ -37,7 +37,7 @@
                         <div class="card-body">
                             <h4 class="card-title">{{trans('finance.fee_payment_report')}}</h4>
                             <p class="card-text font-80pc">{{trans('finance.fee_payment_report_module_description')}}</p>
-                            <router-link to="/finance/fee/report/payment" class="btn btn-info btn-sm">{{trans('general.go_to_link', {link: trans('finance.fee_payment_report')})}}</router-link>
+                            <v-btn to="/finance/fee/report/payment" x-small color="#009efb" dark depressed>{{trans('general.go_to_link', {link: trans('finance.fee_payment_report')})}}</v-btn>
                         </div>
                     </div>
                 </div>
@@ -46,7 +46,7 @@
                         <div class="card-body">
                             <h4 class="card-title">{{trans('finance.transaction_summary_report')}}</h4>
                             <p class="card-text font-80pc">{{trans('finance.transaction_summary_report_module_description')}}</p>
-                            <router-link to="/finance/transaction/report/summary" class="btn btn-info btn-sm">{{trans('general.go_to_link', {link: trans('finance.transaction_summary_report')})}}</router-link>
+                            <v-btn to="/finance/transaction/report/summary" x-small color="#009efb" dark depressed>{{trans('general.go_to_link', {link: trans('finance.transaction_summary_report')})}}</v-btn>
                         </div>
                     </div>
                 </div>
@@ -55,7 +55,7 @@
                         <div class="card-body">
                             <h4 class="card-title">{{trans('finance.transaction_day_book_report')}}</h4>
                             <p class="card-text font-80pc">{{trans('finance.transaction_day_book_report_module_description')}}</p>
-                            <router-link to="/finance/transaction/report/day-book" class="btn btn-info btn-sm">{{trans('general.go_to_link', {link: trans('finance.transaction_day_book_report')})}}</router-link>
+                            <v-btn to="/finance/transaction/report/day-book" x-small color="#009efb" dark depressed>{{trans('general.go_to_link', {link: trans('finance.transaction_day_book_report')})}}</v-btn>
                         </div>
                     </div>
                 </div>
