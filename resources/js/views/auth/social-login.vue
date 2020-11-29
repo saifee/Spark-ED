76bfa2c5d9c6f4a8a0a1cbbaf4bc59a5fb062d28
Salal Aslam <salalaslam@yahoo.com>
2020-11-29 07:28:48 +0500

    format

--- resources/js/views/auth/social-login.vue
+++ resources/js/views/auth/social-login.vue
@@ -1,26 +1,26 @@
 <template>
-    <div class="mb-8">
-        <v-btn 
-            block
-            dark
-            depressed
-            class="mb-1"
-            color="#D44638"
-            @click="authenticate('google')"
-        >
-        Continue with Google
-        </v-btn>
-        <v-btn 
-            block
-            dark
-            depressed
-            class="mb-1"
-            color="#0065b3"
-            @click="authenticate('live')"
-        >
-        Continue with Microsoft
-        </v-btn>
-    </div>
+  <div class="mb-8">
+    <v-btn 
+      block
+      dark
+      depressed
+      class="mb-1"
+      color="#D44638"
+      @click="authenticate('google')"
+    >
+      Continue with Google
+    </v-btn>
+    <v-btn 
+      block
+      dark
+      depressed
+      class="mb-1"
+      color="#0065b3"
+      @click="authenticate('live')"
+    >
+      Continue with Microsoft
+    </v-btn>
+  </div>
 </template>
 
 <script>
